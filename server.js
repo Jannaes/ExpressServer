5dca784a10bcfa953f0493a6f3144dfe250c75ca
Janna Saine <janna.saine@gmail.com>
2025-10-30 10:49:01 +0200

    pin-koodin kysely lisätty

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Express.js palvelin-tiedosto//
 
-const path = require('path')
 const express = require('express')
-
+const path = require('path')
+const fs = require('fs').promises
 const app = express()
 
 // JSON-tiedoston lukeminen
@@ -12,6 +12,22 @@ const herkut = require('./herkut.json')
 app.get('/api/herkut', (req, res) => {  //request ja response
     res.json(herkut)
 })
+
+
+//Pinkoodin lukeminen txt tiedostosta palvelimelta ja lähettäminen selaimelle
+app.get('/api/getpin', async (req, res) => {
+    try {
+        //Luetaan tekstitiedoston sisältö
+    const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+
+    //lähetetään tiedoston sisältö vastauksena
+    res.send(savedPin)
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal Server Error')
+    }
+})
+
 
 // Tehdään polkumääritys public kansioon (staattiset tiedostot)
 const polku = path.join(__dirname, './public')  //(ensimmäinen parametri viittaa mihin liitetään ja toinen mitä liitetään)
